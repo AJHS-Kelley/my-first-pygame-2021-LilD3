a98d361604484eb47c8967fedda78baee7da9ebb
David G <90413176+LilD3@users.noreply.github.com>
2021-11-30 14:39:55 -0500

    v0.3
    setup color values

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First Pygame, David Gay, 11/30/21, 2:29,v0.2
+# My First Pygame, David Gay, 11/30/21, 2:39,v0.3
 
 import pygame, sys
 from pygame. locals import*
@@ -8,5 +8,12 @@ pygame.init()
 
 # setup the game window
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello, world!)
+pygame.display.set_caption('Hello, world!')
 
+# Setup color values.
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED   = (255,0,0)
+GREEN = (0,255,0)
+BLUE  = (0,0,255)
+TANGOBLUE = (0,255,255)
